88ea8d6a65f0276a9393f731101e9d06dada55f6
heygourab <gourabblg901@gmail.com>
2022-04-27 21:21:39 +0530

    ui change-fix 27-4-2022

--- main/studentadd.php
+++ main/studentadd.php
@@ -1,7 +1,45 @@
 <?php
+include '../config/config.php';
 session_start();
+error_reporting(0);
 if (!isset($_SESSION['fullname'])) {
     header("Location: index.php");
+}
+if (isset($_POST['submit'])) {
+    $fname = $_POST['fname']; //first name 
+    $lname = $_POST['lname']; //last name
+
+    $fullname .= $fname;
+    $fullname .= ' ';
+    $fullname .= $lname;
+
+    $fathername = $_POST['fathername']; //father name
+    $mothername = $_POST['mothername']; //mother name
+    $email = $_POST['email'];   //email address
+    $phonenumber = $_POST['phonenumber']; //student phone number
+    $gender = $_POST['gender']; //gender
+    $sm = $_POST['secondary-marks']; //secondary-marks
+    $sp = $_POST['secondary-percentage']; //secondary-Percentage
+    $hsm = $_POST['higher-secondary-marks']; //hs-marks
+    $hsp = $_POST['higher-secondary-percentage']; //hs-percentage
+    $year = $_POST['passout-year']; //year // fix 
+    $board = $_POST['board']; //passout board;
+    $state = $_POST['state']; //state 
+    $pin = $_POST['pin']; //pin 
+    $course = $_POST['course']; //course 
+    $token = bin2hex(random_bytes(15)); // for security //bin2hex converts to hex
+
+    $select_email = mysqli_query($conn, "SELECT * FROM students WHERE email = '" . $_POST['email'] . "'");
+    $select_tel = mysqli_query($conn, "SELECT * FROM students WHERE phonenumber = '" . $_POST['phonenumber'] . "'");
+
+    if (!mysqli_num_rows($select_email) and !mysqli_num_rows($select_tel)) {
+        $sql = "INSERT INTO students(fullname,fathername,mothername,phonenumber,email,gender,secondary_mark,secondary_percentage,higher_secondary_mark,higher_secondary_percentage,passout_year,passout_board,state,pin,course,token)
+                VALUES('$fullname','$fathername','$mothername','$phonenumber','$email','$gender',$sm', '$sp','$hsm' ,'$hsp' , '$year' ,'$board' , '$state' ,'$pin' ,'$course' ,'$token')";
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            echo "<script>alert('Woops!,Something went wrong!!!');</script>";
+        }
+    }
 }
 
 ?>
@@ -82,100 +120,95 @@ if (!isset($_SESSION['fullname'])) {
         <main>
             <h1>Add Student Details</h1>
             <div class="s-form">
-                <form action="" class="form">
+                <form action="" method="POST" class="form">
                     <div class="student-details">
                         <!-- student first name -->
                         <div class="input-box">
                             <span class="details">Student First Name</span>
-                            <input type="text" placeholder="Student First Name" required>
+                            <input type="text" placeholder="Student First Name" name="fname" required>
                         </div>
                         <!-- student last name -->
                         <div class="input-box">
                             <span class="details">Student Last Name</span>
-                            <input type="text" placeholder="Student Last Name" required>
+                            <input type="text" placeholder="Student Last Name" name="lname" required>
                         </div>
                         <!-- student father name -->
                         <div class="input-box">
                             <span class="details">Student's Father Name</span>
-                            <input type="text" placeholder="Student's Father Name" required>
+                            <input type="text" placeholder="Student's Father Name" name="fathername" required>
                         </div>
                         <!-- student  mother name -->
                         <div class="input-box">
                             <span class="details">Student's Mother Name</span>
-                            <input type="text" placeholder="Student's Mother Name" required>
+                            <input type="text" placeholder="Student's Mother Name" name="mothername" required>
                         </div>
                         <!-- student email  -->
                         <div class="input-box">
                             <span class="details">Student's Email Address</span>
-                            <input type="email" placeholder="Student's Email Address" required">
+                            <input type="email" placeholder="Student's Email Address" name="email" required>
                         </div>
                         <!-- student phonenumber -->
                         <div class="input-box">
                             <span class="details">Student's Contact Number</span>
                             <input type="tel" placeholder="Student's Contact Number" pattern="[0-9]{10}" maxlength="10"
-                                required>
+                                name="phonenumber" required>
                         </div>
-                    </div>
-                    <!-- gender-details -->
-                    <div class="gender-details">
-                        <input type="radio" name="gender" value="" id="dot-1">
-                        <input type="radio" name="gender" value="" id="dot-2">
-                        <input type="radio" name="gender" value="" id="dot-3">
-                        <span class="gender-title">
-                            <h2>Student's Gender</h2>
-                        </span>
-                        <div class="category">
-                            <!-- male -->
-                            <label for="dot-1">
-                                <span class="dot one"></span>
-                                <span class="gender">Male</span>
-                            </label>
-                            <!-- female -->
-                            <label for="dot-2">
-                                <span class="dot two"></span>
-                                <span class="gender">Female</span>
-                            </label>
-                            <!-- other -->
-                            <label for="dot-3">
-                                <span class="dot three"></span>
-                                <span class="gender">Other</span>
-                            </label>
+
+                        <!-- gender-details -->
+                        <div class="input-box">
+                            <span class="details">Student's Gender</span>
+                            <select name="course" id="course" class="form-control" required>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Other">Other</option>
+                            </select>
                         </div>
-                    </div>
-                    <!-- end of gender section-->
-                    <div class="student-details">
+                        <!-- student course -->
+                        <div class="input-box">
+                            <span class="details">Student's Course</span>
+                            <select name="course" id="course" class="form-control" required>
+                                <option value="BCA(H)">BCA(H)</option>
+                                <option value="BHM(H)">BHM(H)</option>
+                                <option value="BBA(H)">BBA(H)</option>
+                            </select>
+                        </div>
+                        <!-- end of gender section-->
+
                         <!-- student secondary-marks-->
                         <div class="input-box">
                             <span class="details">Student Secondary Marks</span>
-                            <input type="number" placeholder="Student's Secondary Marks" min="1" max="700" required>
+                            <input type="number" placeholder="Student's Secondary Marks" min="1" max="700"
+                                name="secondary-marks" required>
                         </div>
                         <!-- Student Secondary Percentage -->
                         <div class="input-box">
                             <span class="details">Student Secondary Percentage</span>
                             <input type="number" placeholder="Student's Secondary Percentage" min="1" max="100"
-                                required>
+                                name="secondary-percentage" required>
                         </div>
                         <!-- Student's Highest Secondary Marks -->
                         <div class="input-box">
                             <span class="details">Student's Highest Secondary Marks</span>
-                            <input type="number" placeholder="Student's Highest Secondary Marks" min="1" max="500"
-                                required>
+                            <input type="number" placeholder="Student's Higher Secondary Marks" min="1" max="500"
+                                name="higher-secondary-marks" required>
                         </div>
                         <!-- Student's Highest Secondary Percentage -->
                         <div class="input-box">
                             <span class="details">Student's Highest Secondary Percentage</span>
                             <input type="number" placeholder="Student's Highest Secondary Percentage" min="1" max="100"
-                                required>
+                                name="higher-secondary-percentage" required>
                         </div>
                         <!-- Student's Highest Secondary Passout Year -->
                         <div class="input-box">
                             <span class="details">Student's HS Passout Year (month-year)</span>
-                            <input type="month" placeholder="Student's Highest Secondary Passout Year" required>
+                            <input type="text" placeholder="Student's Highest Secondary Passout Year"
+                                name="passout-year" pattern="[0-9]{4}" required>
                         </div>
                         <!-- student's Highest Secondary Board-->
                         <div class="input-box">
                             <span class="details">Student's HS Passout Board</span>
-                            <input type="text" placeholder="Student's HS Passout Board (e.g-cbsc)" required>
+                            <input type="text" placeholder="Student's HS Passout Board (e.g-cbsc)" name="board"
+                                required>
                         </div>
                         <!-- student's state -->
                         <div class="input-box">
@@ -225,25 +258,11 @@ if (!isset($_SESSION['fullname'])) {
                             <input type="text" name="pin" pattern="[0-9]{6}" maxlength="6"
                                 placeholder="Student's Pincode (e.g: 733101)" required>
                         </div>
-                        <!-- student course -->
-                        <div class="input-box">
-                            <span class="details">Student's Course</span>
-                            <select name="course" id="course" class="form-control" required>
-                                <option value="BCA(H)">BCA(H)</option>
-                                <option value="BHM(H)">BHM(H)</option>
-                                <option value="BBA(H)">BBA(H)</option>
-                            </select>
-                        </div>
-                        <!-- submit-->
-                        <div class="input-box">
-                            <div class="btn-container">
-                                <button class="button" name="submit">SUBMIT</button>
-                            </div>
-                        </div>
-
                     </div>
-
-
+                    <!-- submit-->
+                    <div class="btn-container">
+                        <button class="button" name="submit">SUBMIT</button>
+                    </div>
                 </form>
             </div>
         </main>
