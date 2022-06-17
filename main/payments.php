43af38fbb9dba602c0bec085dbac880a6d9e228e
heygourab <gourabblg901@gmail.com>
2022-06-18 01:52:57 +0530

    update

--- main/payments.php
+++ main/payments.php
@@ -94,10 +94,105 @@ if (!isset($_SESSION['fullname'])) {
                     <H3>Logout!</H3>
                 </a>
             </div>
-
         </aside>
         <!-- End of Aside -->
+        <main>
+            <h1>All Payments Information</h1>
+            <div class ='payments-info'>
+                <h2>
+                    Recent Student 
+                </h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Student Name
+                            </th>
+                            <th>
+                                Student Course
+                            </th>
+                            <th>
+                                Enter Fees Amount
+                            </th>
+                            <th>
+                                Select Payment Status
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td>
+                            <h3>
+                                Gourab Sarkar
+                            </h3>
+                        </td>
+                        <td>
+                            <h3>
+                                BCA(H)
+                            </h3>
+                        </td>
+                        <td>
+                            <div class="input-box">
+                                <input type="number" placeholder="Enter Student Fees Amount" name="secondary-mark" required>
+                            </div>
+                        </td>
+                        <td>
+                            <select name="payment" id="payment" class="form-control" required>
+                                <option value="Male">Panding</option>
+                                <option value="Female">Done</option>
+                                <option value="Other">Unsuccessful</option>
+                            </select>
+                        </td>
+                    </tbody>
+                    
+                </table>
+            </div>
+        </main>
+        <div class="right">
+            <div class="top">
+                <!-- menu-dtn -->
+                <button id="menu-dtn">
+                    <span class="material-icons-sharp"> menu </span>
+                </button>
+                <!-- profile -->
+                <div class="profile">
+                    <div class="info">
+                        <p>
+                            <b>
+                                <h2>
+                                    Hello,
+                                    <?php echo $_SESSION['fullname'] ?>
+                                </h2>
+                            </b>
+                        </p>
+                        <small class="text-muted">Admin</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="notes">
+                <h2>Notes</h2>
+                <div class="note">
+                    <div class="message">
+                        <p>
+                            <b>
+                                Student Payment Update </b>
+                            </b>
+                            Don't worry use the update button for update the student payment.
+                        </p>
+                    </div>
+                    <div class="message">
+                        <p>
+                            <b>
+                                Studnent Payment Status Search </b>
+                            </b>
+                            Use the Search button for Student Payment Status Search.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 </body>
 
 </html>
